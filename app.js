3876f5824b9d07f275aa13ff625d9b733131253f
Tas Yusoontorn <mac@DN51u5ng.SUNet>
2018-05-31 10:02:21 -0700

    fix kTC and double add

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-mongoose.connect("mongodb://localhost:27017/abigslip");
-// mongoose.connect("mongodb://test:test@ds016298.mlab.com:16298/abigslip");
+// mongoose.connect("mongodb://localhost:27017/abigslip");
+mongoose.connect("mongodb://test:test@ds016298.mlab.com:16298/abigslip");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -138,7 +138,7 @@ function seedDB() {
     {
       title: 'เคมีม.ปลาย',
       code: 5,
-      numBook: 3,
+      numBook: 4,
       bookCodes: [15,16,17,18,19,20,22,23,24,25,26,27,28,29,30]
     },
     {
